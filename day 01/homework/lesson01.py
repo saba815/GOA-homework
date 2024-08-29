c571ec1637fc7acebb8a3f0156391511a4343427
saba815 <sabunasesitashvili@gmail.com>
2024-08-29 16:49:03 +0400

    second commit

---
+++ day 01/homework/lesson01.py
@@ -0,0 +1,17 @@
+name = "saba"
+
+
+surname = "sesitashvili"
+
+print(name)
+
+name = "saba"
+age = 9
+height = 1.35
+knows_programming = True
+is_ugly = False
+#print(name +" " + surname)
+print(name + str (age) )
+#print(type (age) )
+#print(type (height) )
+#print(type (knows_programming) )
