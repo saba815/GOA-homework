6edb250cc8c3b88e89763409d551e9df9e273cce
saba815 <sabunasesitashvili@gmail.com>
2024-10-09 00:48:22 +0400

    commit

---
+++ day 04/classwork/task04.py
@@ -0,0 +1,5 @@
+first=850
+second=900
+print(second + first)
+print(second - first)
+print(second + first - second * first // second)
---
+++ day 04/homework/lesson04.py
@@ -0,0 +1,5 @@
+first=789
+second=910
+third=200
+print(first + second - third)
+print(first * second / third)
