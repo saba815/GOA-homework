8aad67c8f3c7a6c84b9f9214da174283f96a7cd6
saba815 <sabunasesitashvili@gmail.com>
2024-11-18 23:24:56 +0400

    commit

---
+++ day 12/homework/lesson12.py
@@ -0,0 +1,21 @@
+# ცვლადში სახელად number-ში ვინახავთ მომხმარებლის შემოტანილ ინფორმაციას.
+
+number=int(input("enter your number:" ))
+
+#ვამრავლებთ შემოტანილ ინფორმაციას 2-ზე
+
+number2=(number * 2)
+
+print("your number multiplied by 2 is "+ str(number2)) 
+
+number1=int(input("enter your number1:" ))
+number3=int(input("enter your number3: "))
+
+print(number1-number3)
+print(number3+number1)
+print(number1 * number3)
+print(number1//number3)
+
+#str() აქცევს ყველა ელემენტს სტრინგად.
+#int() აქცევს ყველა ელემენტს ინტეგერად.
+#float() აქცევს ყველაფერს ათწილა რიცხვად.
