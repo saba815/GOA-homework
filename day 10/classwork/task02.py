aa741989af6efabe9486d469ecab8f4331f94c33
saba815 <sabunasesitashvili@gmail.com>
2024-12-03 21:32:44 +0400

    commit

--- day 10/classwork/task02.py
+++ day 10/classwork/task02.py
@@ -1,8 +1,21 @@
-#3) შექმენით პროგრამა, რომელშიც მომხმარებელს შეეკითხებით და input-ის გამოყენებით შემოატანინებთ სახელსა და ასაკს, შემდეგ კი დაბეჭდავთ მათ მონაცემთა ტიპებს
+#მომხმარებლის სახელის და სხა რეღაცეების შემოტანა
+
+name=input("enter your name: ")
+last_name=input("enter your last_name: ")
+age=input("enter your age: ")
+email=input("enter your email: ")
+password=input("enter your password: ")
+hobby=input("enter your hobby: ")
+most_loved_sport=input("enter your most_loved_sport: ")
 
 
-name=input("enter your name:")
-age=input("enter your age:")
 
-print(type(name))
-print(type(age))
+print(name)
+print(last_name)
+print(age)
+print(email)
+print(password)
+print(hobby)
+print(most_loved_sport)
+
+print(name, last_name, age, email,  password,  hobby,  most_loved_sport)
