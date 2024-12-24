403aff1a9942260a9952b0d0549f50acf94faa93
saba815 <sabunasesitashvili@gmail.com>
2024-12-24 22:30:53 +0400

    commit

---
+++ day 16/homework/lesson16.py
@@ -0,0 +1,18 @@
+boolean_შედარებები1=(8>0) #True
+boolean_შედარებები2=(9>3) #True
+boolean_შედარებები3=(10<1) #False
+boolean_შედარებები4=(30<4) #False
+boolean_შედარებები5=(567<90) #False
+
+boolean_logical_operation1=(False and True)
+boolean_logical_operation2=(True and False)
+boolean_logical_operation3=(False or True)
+boolean_logical_operation4=(True or False)
+boolean_logical_operation=(not False)
+
+
+read_pages=23
+free_time=True
+productive=(read_pages + free_time)
+
+print(productive)
