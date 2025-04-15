f4bcb7f532891f99ac3f60b3db76b38f80640f2f
saba815 <sabunasesitashvili@gmail.com>
2025-04-15 11:52:06 -0700

    commit

---
+++ day 34/classwork/task03.py
@@ -0,0 +1,7 @@
+product1 = input("შეიყვანე პირველი პროდუქტი: ")
+product2 = input("შეიყვანე მეორე პროდუქტი: ")
+product3 = input("შეიყვანე მესამე პროდუქტი: ")
+
+sia = [product1, product2, product3]
+
+print(sia)
