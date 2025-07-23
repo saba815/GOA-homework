20907855b0845f90f39eec6c68f526bdab9866f4
saba815 <sabunasesitashvili@gmail.com>
2025-07-23 12:12:41 -0700

    commit

---
+++ day 04/classwork/task01.py
@@ -0,0 +1,74 @@
+num1 = 5
+num2 = 8
+num3 = num1 + num2
+print(num3)
+
+#these are incorrect codes
+
+# name4 = name2 + surname
+
+# print(name4)
+
+# name0 = surname+ name1
+
+# print(name0)
+
+# math = "6" + 6
+
+# print(math)
+
+# city3 = city1 + city2
+
+# print(city3)
+
+# box_of_memory = memory1 + memory2
+
+# print(box_of_memory)
+
+#now here is the correct codes
+
+name1 = "saba"
+name2 = "giorgi"
+surname = "sesitashvili"
+
+name4 = name2 + surname
+
+print(name4)
+
+name0 = surname + name1
+
+print(name0)
+
+math = 6 + 6
+
+print(math)
+
+city1 = "manchester"
+city2 = "city"
+city3 = city1 + city2
+
+print(city3)
+
+memory1 = "my first trip"
+memory2 = "to bakuriani"
+
+box_of_memories = memory1 + memory2
+
+
+print(box_of_memories)
+
+#now the problems explained
+
+#first code - there wasnt any name 2 or surname so where did it take that information from? we didint put "" so it wasnt a string 
+# it was a variable.
+
+#second code - the second code had the same problem as the first code just a diffrent name and diffren variable.
+
+#the third code - You could not add an integer to a string that was the problem.
+
+#the fourth code - it had the same problem as the first and the second code the only diffrence is that the variables were 
+# named city.
+
+#the last code - the same problem as the first second and the fourth just diffren names.
+
+#დაბაგვა ნიშნავს პროგრამაში შეცდომების პოვნასა და გამოსწორებას.
---
+++ day 04/homework/lesson04.py
@@ -0,0 +1,29 @@
+points = 50        # თავდაპირველი მნიშვნელობა
+points = points + 20   # პირველი განახლება
+points = points - 10   # მეორე განახლება
+print(points)      # საბოლოო მნიშვნელობის დაბეჭდვა
+
+
+
+#this is a code taht is incorrect
+# name = input("რა არის შენი სახელი?)
+# print("მოგესალმები", name)
+# age = int(input("რამდენი წლის ხარ?"))
+# if age > 18
+# print("სრულწლოვანი ხარ")
+# else
+# print("არ ხარ სრულწლოვანი")
+# fav_color = inpt("შენი საყვარელი ფერი?")
+# print("შენი ფერია", fav_color)
+
+
+#this is the correct version of this code
+name = input("რა არის შენი სახელი?")  # ბრჭყალების დახურვა
+print("მოგესალმები", name)
+age = int(input("რამდენი წლის ხარ?"))
+if age > 18:
+    print("სრულწლოვანი ხარ")         # if ბლოკი სწორად ჩამოყალიბებულია
+else:
+    print("არ ხარ სრულწლოვანი")       # else ბლოკიც სწორად
+fav_color = input("შენი საყვარელი ფერი?")  # inpt -> input
+print("შენი ფერია", fav_color)
